570355272c7323843d9af407671179dfb9f13691
Stefan Tosic <tosic.ra29.2019@uns.ac.rs>
2022-06-17 22:12:46 +0200

    add admin user in AdministratorRepository.json

---
+++ Projekat/src/repository/AdministratorRepository.java
@@ -0,0 +1,20 @@
+package repository;
+
+import beans.User;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class AdministratorRepository extends Repository<User, String>{
+
+    @Override
+    protected String getKey(User user) {
+        return user.getUsername();
+    }
+
+    @Override
+    protected Type getTokenType() {
+        return new TypeToken<ArrayList<LogicalEntity<User>>>() {}.getType();
+    }
+}
--- Projekat/src/service/CustomerService.java
+++ Projekat/src/service/CustomerService.java
@@ -1,13 +1,16 @@
 package service;
 
 import beans.Customer;
+import beans.Role;
 import dto.CustomerDTO;
+import repository.AdministratorRepository;
 import repository.CustomerRepository;
 
 import java.util.List;
 
 public class CustomerService {
     private CustomerRepository customerRepository = new CustomerRepository();
+    private AdministratorRepository administratorRepository = new AdministratorRepository();
 
     public List<Customer> getAll(){
         return customerRepository.getAll();
@@ -21,8 +24,16 @@ public class CustomerService {
         customer.setFirstName(customerDTO.getFirstName());
         customer.setLastName(customerDTO.getLastName());
         customer.setDob(customerDTO.getDob());
+        customer.setRole(Role.Customer);
         customer.setGender(customerDTO.getGender());
 
         return customerRepository.addOne(customer);
+
+//        User admin = new User();
+//        admin.setUsername("admin");
+//        admin.setPassword("123admin456");
+//        admin.setRole(Role.Administrator);
+//
+//        return administratorRepository.addOne(admin);
     }
 }
